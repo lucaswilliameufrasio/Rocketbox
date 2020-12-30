ec54be5d6973c6b41db60e79dbb1b55593919490
Lucas William <lucaswilliameufrasio@gmail.com>
2020-12-29 22:02:55 -0300

    chore: rename app

--- mobile/android/app/src/main/java/com/rocketbox/MainApplication.java
+++ mobile/android/app/src/main/java/com/rocketbox/MainApplication.java
@@ -1,4 +1,4 @@
-package com.mobile;
+package com.rocketbox;
 
 import android.app.Application;
 import android.content.Context;
--- mobile/ios/RocketBoxTests/RocketBoxTests.m
+++ mobile/ios/RocketBoxTests/RocketBoxTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobileTests : XCTestCase
+@interface RocketBoxTests : XCTestCase
 
 @end
 
-@implementation mobileTests
+@implementation RocketBoxTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
